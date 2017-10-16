746f545042d4f2faeb7ef18a5c75c6a56311e524
Samya <samyaahsaan@gmail.com>
2017-10-16 15:24:17 -0500

    iosdev

--- TipPro/AppDelegate.swift
+++ TipPro/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("hello Make School!")
+        print("how is this working???")
         return true
     }
 
